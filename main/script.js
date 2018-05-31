c6b63f7b2e5388e42693e0c00a63e60c0e526977
StanislavZozulya <stas_zozulya@mail.ru>
2018-05-31 21:10:47 +0300

    Added first functions for cells

--- main/script.js
+++ main/script.js
@@ -14,3 +14,30 @@ var fontSize;
 var loss = false;
 
 startGame();
+
+changeSize.onclick = function () {
+	if (sizeInput.value >= 2 && sizeInput.value <= 20) {
+		size = sizeInput.value;
+		width = canvas.width / size - 6;
+		console.log(sizeInput.value);
+		canvasClean();
+		startGame();
+	}
+}
+
+function cell(row, coll) {
+	this.value = 0;
+	this.x = coll * width + 5 * (coll + 1);
+	this.y = row * width + 5 * (row + 1);
+}
+
+function createCells() {
+	var i, j;
+	for(i = 0; i < size; i++) {
+		cells[i] = [];
+		for(j = 0; j < size; j++) {
+			cells[i][j] = new cell(i, j);
+		}
+	}
+}
+
