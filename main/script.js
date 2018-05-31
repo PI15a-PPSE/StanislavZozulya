c65ea2aa79449c6584a458dddd59329ae906eea1
StanislavZozulya <stas_zozulya@mail.ru>
2018-05-31 22:38:45 +0300

    Added up and down movement

--- main/script.js
+++ main/script.js
@@ -188,3 +188,55 @@ function moveLeft() {
 	}
 	pasteNewCell();
 }
+
+function moveUp() {
+	var i, j, row;
+	for(j = 0; j < size; j++) {
+		for(i = 1; i < size; i++) {
+			if(cells[i][j].value) {
+				row = i;
+				while (row > 0) {
+					if(!cells[row - 1][j].value) {
+						cells[row - 1][j].value = cells[row][j].value;
+						cells[row][j].value = 0;
+						row--;
+					} else if (cells[row][j].value == cells[row - 1][j].value) {
+						cells[row - 1][j].value *= 2;
+						score +=  cells[row - 1][j].value;
+						cells[row][j].value = 0;
+						break;
+					} else {
+						break; 
+					}
+				}
+			}
+		}
+	}
+	pasteNewCell();
+}
+
+function moveDown() {
+	var i, j, row;
+	for(j = 0; j < size; j++) {
+		for(i = size - 2; i >= 0; i--) {
+			if(cells[i][j].value) {
+				row = i;
+				while (row + 1 < size) {
+					if (!cells[row + 1][j].value) {
+						cells[row + 1][j].value = cells[row][j].value;
+						cells[row][j].value = 0;
+						row++;
+					} else if (cells[row][j].value == cells[row + 1][j].value) {
+						cells[row + 1][j].value *= 2;
+						score +=  cells[row + 1][j].value;
+						cells[row][j].value = 0;
+						break;
+					} else {
+						break; 
+					}
+				}
+			}
+		}
+	}
+	pasteNewCell();
+}
